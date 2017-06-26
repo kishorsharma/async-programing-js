24bbc09028a4dfaf60cf841610e5a73cbbeb625f
Brij Kishor <kishorsharma7@gmail.com>
2017-06-26 15:56:46 +0530

    Reverting back ex to empty state.

--- thunks_ex/ex2.js
+++ thunks_ex/ex2.js
@@ -1,28 +1,8 @@
 var util = require('../util/util');
 
 function getFile(file) {
-    var fn, text;
-    util.fakeAjax(file, function(data) {
-        if (!fn) text = data;
-        else fn(data);
-    });
-    return function th(cb) {
-        if (text) cb(text)
-        else fn = cb;
-    }
+    // what do we do here?
 }
 
-var th1 = getFile("File1");
-var th2 = getFile("File2");
-var th3 = getFile("file3");
-
-th1(function(data1) {
-    util.output(data1);
-    th2(function(data2) {
-        util.output(data2);
-        th3(function(data3) {
-            util.output(data3);
-            util.output("Complete");
-        })
-    });
-})
+// request all files at once in "parallel"
+// ???
