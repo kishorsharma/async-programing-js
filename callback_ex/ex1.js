5fc01eaa5efe636996d4b507acd57f9ab09dca4b
Brij Kishor <kishorsharma7@gmail.com>
2017-06-26 15:54:50 +0530

    Reverting back ex to empty state.

--- callback_ex/ex1.js
+++ callback_ex/ex1.js
@@ -5,24 +5,8 @@ var util = require('../util/util');
 
 function getFile(file) {
     util.fakeAjax(file, function(text) {
-        respState[file] = text;
-        print(file, text);
+        // ???
     });
-}
-var respState = {};
-
-function saveState(url, resp) {
-    if (url === "file3") {
-        respState[url] = resp;
-        if (respState["file2"]) {
-
-        }
-    }
-
-    if (respState[url]) {
-        util.output(resp);
-        resp[file] = null;
-    }
 }
 
 // request all files at once in "parallel"
